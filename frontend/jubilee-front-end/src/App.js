7ef52ac948befc1487afe47a611f389e5ebb1b90
Jordis Man <jordisman@gmail.com>
2020-09-18 12:59:00 -0700

    Added background and title

--- frontend/jubilee-front-end/src/App.js
+++ frontend/jubilee-front-end/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import styles from './styles.module.css';
 
 function App() {
   return (
     <div>
-      Testing
+      <div className={styles.container}>
+        <div className={styles.titleBox}>
+          <h1 className={styles.title}>
+            <span className={styles.titleMain}>JUBILEE</span>
+          </h1>
+          <p className={styles.titleSub}>
+            Having trouble remembering a song?
+          </p>
+          <p className={styles.titleSub}>
+            Type whatever line you remember and we’ ll help you out
+          </p>
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
