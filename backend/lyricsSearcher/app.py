ca9e264ce3ee473161433ac484db92be2f1b4486
Chenqin Zhang <Renyqin@users.noreply.github.com>
2020-09-19 18:30:20 +1000

    lyrics searcher is ready

--- backend/lyricsSearcher/app.py
+++ backend/lyricsSearcher/app.py
@@ -8,26 +8,26 @@ from jina.flow import Flow
 
 
 def config():
+    # delete data file later
     os.environ["JINA_DATA_FILE"] = os.environ.get(
-        "JINA_DATA_FILE", "data/character-lines.csv"
+        "JINA_DATA_FILE", "../db/all-lyrics.txt"
     )
     os.environ["JINA_WORKSPACE"] = os.environ.get("JINA_WORKSPACE", "workspace")
 
     os.environ["JINA_PORT"] = os.environ.get("JINA_PORT", str(45678))
 
-# for reference. Delete later
+
 def print_topk(resp, sentence):
     for d in resp.search.docs:
-        print(f"Ta-Dah🔮, here are what we found for: {sentence}")
+        print(f"🔮This might be the Songs you searched for with lyrics: {sentence}")
         for idx, match in enumerate(d.matches):
 
             score = match.score.value
             if score < 0.0:
                 continue
-            song_name = match.meta_info.decode()
+            name = match.meta_info.decode()
             lyrics = match.text.strip()
-            print(f'> {idx:>2d}({score:.2f}). {song_name.upper()} :, "{lyrics}"')
-
+            print(f'> {idx:>2d}({score:.2f}). {name.upper()} :  "{lyrics}"')
 
 def index(num_docs):
     f = Flow().load_config("flow-index.yml")
@@ -40,38 +40,18 @@ def index(num_docs):
         )
 
 
-# Integrate to front end
-def send_topk_song(resp, sentence):
-    for d in resp.search.docs:
-        print(f"Ta-Dah🔮, here are what we found for: {sentence}")
-        for idx, match in enumerate(d.matches):
-
-            score = match.score.value
-            if score < 0.0:
-                continue
-            song_name = match.meta_info.decode()
-            lyrics = match.text.strip()
-            # TODO: send song_name and lyrics.
-
-            print(f'> {idx:>2d}({score:.2f}). {song_name.upper()} :, "{lyrics}"')
-
-
-# Integrate to front end.
-def query_from_frontend(top_k = 1):
+"""def query(top_k):
     f = Flow().load_config("flow-query.yml")
     with f:
         while True:
-            # TODO: geting searching text from frontend.
-            text = ""
+            text = input("please type a sentence: ")
             if not text:
                 break
 
             def ppr(x):
-                send_topk_song(x, text)
-            f.search_lines(lines=[text, ], output_fn=ppr, top_k=top_k)
+                print_topk(x, text)
+            f.search_files([text, ], output_fn=ppr, top_k=top_k)"""
 
-
-# for reference. Delete later
 def query(top_k):
     f = Flow().load_config("flow-query.yml")
     with f:
@@ -92,7 +72,6 @@ def query_restful():
         f.block()
 
 
-
 def dryrun():
     f = Flow().load_config("flow-index.yml")
     with f:
@@ -109,14 +88,12 @@ def dryrun():
 )
 @click.option("--num_docs", "-n", default=50)
 @click.option("--top_k", "-k", default=5)
-
-
 def main(task, num_docs, top_k):
     config()
     if task == "index":
         index(num_docs)
     if task == "query":
-        query_from_frontend()
+        query(top_k)
     if task == "query_restful":
         query_restful()
     if task == "dryrun":
