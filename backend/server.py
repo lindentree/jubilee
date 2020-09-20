3e9fa21997bebc403e82c4ded6b0c007d7b7de66
Linden Chiu <hchiu87@gmail.com>
2020-09-19 19:47:12 -0700

    frontend integration

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, jsonify, send_from_directory)
+from flask_cors import CORS
+
 
 app = Flask(__name__, static_folder="../frontend/jubilee-front-end/build/static", template_folder="../frontend/jubilee-front-end/build")
 
@@ -10,10 +12,13 @@ def my_index():
     return render_template("index.html") #flask_token="Hello   world")
 
 
-@app.route('/api/items')
-def items():
-  '''Sample API route for data'''
-  return jsonify([{'title': 'A'}, {'title': 'B'}])
+# @app.route('/api/items')
+# def items():
+    # data = request.get_json()
+    # print(data)
+    # return data
+#   '''Sample API route for data'''
+#   return jsonify([{'title': 'A'}, {'title': 'B'}])
 
 
 app.run(debug=True)
