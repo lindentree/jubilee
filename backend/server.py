11955e92b6ab2f869ddcca530e32c60f705ce5bf
Linden Chiu <hchiu87@gmail.com>
2020-09-19 18:50:23 -0700

    working flask server

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,19 @@
-from flask import Flask
+#!/usr/bin/env python3
 
-app = Flask(__name__)
+import os
+from flask import (Flask, render_template)
+
+app = Flask(__name__, static_folder="../frontend/jubilee-front-end/build/static", template_folder="../frontend/jubilee-front-end/build")
+
+@app.route("/")
+def my_index():
+    return render_template("index.html") #flask_token="Hello   world")
+
+
+@app.route('/api/items')
+def items():
+  '''Sample API route for data'''
+  return jsonify([{'title': 'A'}, {'title': 'B'}])
+
+
+app.run(debug=True)
